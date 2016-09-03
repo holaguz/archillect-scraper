4e664b8a3386552e8c54743561f6032a056c63dc
holaguz <syx0ww@gmail.com>
2016-09-02 22:27:16 -0300

    create img dir on first run

--- scraper.py
+++ scraper.py
@@ -8,33 +8,11 @@ import configparser
 config = configparser.ConfigParser()
 config.read('./config.cfg')
 
-
-
 archillectIndex = int(config['DEFAULT']['lastIndex'])
 #minWidth = ''
 #minHeight = ''
 
-
-## open file for index recovery + parameters
-'''
-f = open("config.cfg", 'rt')
-
-## load config
-
-while (archillectIndex=='' or archillectIndex=="\n" or archillectIndex[:1]=="#"):  ## checks if the var is null, an empty line or a comment
-	archillectIndex = f.readline().rstrip()
-archillectIndex = int(archillectIndex)
-
-while (minWidth == "" or minWidth == "\n" or minWidth[:1]=='#'):
-	minWidth = f.readline().rstrip()
-
-while (minHeight == '' or minHeight=='\n' or minHeight[:1]=='#'):
-	minHeight = f.readline().rstrip()
-
-f.close()
-'''
-
-
+if(not os.path.exists('img')): os.makedirs('img')
 
 ## initialize browser for parsing
 
@@ -49,11 +27,11 @@ def scrapeImg(index):
 	tree = html.fromstring(r.content)
 
 	try:
-		link = tree.xpath('//*[@id="ii"]/@src')[0] ## [0]: retrieves the first element matching the criteria
-		extIndex = link.rfind(".",0) 				##search backwards looking for the first "."
-		extention = link[extIndex:]		## retrieves the file extention
-		
-		open('img/' + index + extention , 'r') # raises FileNotFoundError exception if the file is doesn't exist
+		link = tree.xpath('//*[@id="ii"]/@src')[0] 			## [0]: retrieves the first element matching the criteria
+		extIndex = link.rfind(".",0) 						## search backwards looking for the first "."
+		extention = link[extIndex:]							## retrieves the file extention
+
+		open('img/' + index + extention , 'r') 				## raises FileNotFoundError exception if the file is doesn't exist
 		return 1;
 
 	except FileNotFoundError:
@@ -67,7 +45,7 @@ def scrapeImg(index):
 
 def saveImg(link, filename, extention):
 
-	img = requests.get(link) #load the image to memory
+	img = requests.get(link) 								## load the image to memory
 
 	fileImg = open('img/' +filename + extention, 'wb')
 	fileImg.write(img.content)
@@ -84,13 +62,11 @@ while(0==0):
 
 	if(scrapeImg(archillectIndex) == 1):
 		archillectIndex = archillectIndex + 1
-		eraseFile(archillectIndex - 61);
+		eraseFile(archillectIndex - 61);					## i just want to keep the last 60 imgs. need to make this editable
 
 	else:
 		config['DEFAULT']['lastIndex'] = str(archillectIndex)
 		config.write(open('./config.cfg','w'));
 		print('Quiting in 5...')
-		#time.sleep(5);
+		time.sleep(5);
 		break;
-	
-	
