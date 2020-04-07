e30a12540f0bb7dd948fd1f6eaa512045b765cfe
agus <syx0ww@gmail.com>
2020-04-06 21:08:50 -0300

    cleanup

--- scraper.py
+++ scraper.py
@@ -11,88 +11,88 @@ config.read('./config.cfg')
 ndx = int(config['DEFAULT']['lastIndex'])
 imgCount = int(config['DEFAULT']['imgCount'])
 baseAddress = 'http://archillect.com/'
+img_dir = "./img/"
 #minWidth = ''
 #minHeight = ''
 
-if(not os.path.exists('img')): os.makedirs('img')
+if not os.path.exists(img_dir): os.makedirs(img_dir)
 
-## initialize browser for parsing
-
-def scrapeImg(index, save):
+def scrapeImg(index):
 
 	index = str(index)
 
 	url = baseAddress + index
-	print('Scraping ' + url + '...')
-
 	r = requests.get(url)
 	tree = html.fromstring(r.content)
 
 	try:
-		link = tree.xpath('//*[@id="ii"]/@src')[0] 			## [0]: retrieve the first element matching the criteria
-		extIndex = link.rfind(".",0) 						## search backwards looking for the first "."
-		filenameExtention = link[extIndex:]							## retrieve the file filenameExtention
-		if save: open('img/' + index + filenameExtention , 'r') 				## raise FileNotFoundError exception if the file don't exist
-		return 1;
-
-	except FileNotFoundError:
-		print('Saving...')
-		saveImg(link, index, filenameExtention)
-		return 1;
+		link = tree.xpath('//*[@id="ii"]/@src')[0] 								## [0]: retrieve the first element matching the criteria
+		extIndex = link.rfind(".",0) 											## search backwards looking for the first "."
+		fileExtention = link[extIndex:]											## retrieve the file basetype
+		fileName = index + fileExtention
+		return fileName;
 
 	except IndexError:
-		print('[ERR]: Looks like '+ baseAddress + str(index) +' is not valid.')
-		return 0;
+		print('Looks like '+ baseAddress + str(index) +' is not valid.')
+		return -1;
 
-def saveImg(link, filename, filenameExtention):
-	img = requests.get(link) 								## load the image to memory
-	fileImg = open('img/' + filename + filenameExtention, 'wb')
-	fileImg.write(img.content)
-	fileImg.close();
+def saveImg(fileName):
+
+	if fileName in os.listdir(img_dir): return 0;
+
+	fileAddress = baseAddress + fileName
+	print("Saving " + fileAddress)
+	file = requests.get(fileAddress) 											## load the file to memory
+	
+	with open(img_dir + fileName, 'wb') as f: 
+		f.write(file.content)
+		f.close();
 
 def eraseFiles():
 	try:
 		fileList = os.listdir("./img")
 		while(len(fileList) > imgCount):
+			
 			os.remove("./img/" + fileList[0])
 			fileList = os.listdir("./img")
-		#path = glob.glob('img/' + str(filename) + '.*')[0]
-		#os.remove(path);
 	except: pass
 
 def searchIndex():
-	#iterate over indexes on the website
+
 	step = imgCount;
 	index = ndx;
 	
 	while(True):
-		print("\nSearching last index... Current index: " + str(index) + " step " + str(step))
-
-		if(scrapeImg(index, False) == 0):  # Si el indice no es valido vuelvo hacia atras y reduzco el paso
+		print("Searching last index... Current index: " + str(index) + " step " + str(step) + "... ", end='')
+		if scrapeImg(index) == -1:  											## when the index is not valid we go backwards
 			index -= step
 			step = int(step/2)
-			if step == 1 : return index
+			if step == 0 : return index
 		else: 
 			index += step;
 			print('Index is valid, continue search')
 
 
-
-
-ndx = searchIndex()
+ndx = searchIndex()																## main loop
 print('Found index: ' + str(ndx))
+ndx -= imgCount
 
 while(True):
 	
-	if(scrapeImg(ndx, True) == 1):
-		ndx = ndx + 1
-		
+	f = scrapeImg(ndx)
+	print('Scraping ' + baseAddress + str(ndx) + '...')
+
+	if f != -1:
+		saveImg(f)
+		ndx += 1
 	else:
-		config['DEFAULT']['lastIndex'] = str(ndx)
-		config['DEFAULT']['imgCount'] = str(imgCount)
-		config.write(open('./config.cfg','w'));
 		print("Erasing extra files...")
 		eraseFiles();
+
+		config['DEFAULT']['lastIndex'] = str(ndx)
+		#config['DEFAULT']['imgCount'] = str(imgCount)
+		config.write(open('./config.cfg','w'));
+		
 		print('Quiting in 5...')
 		##time.sleep(5);
 		break;
